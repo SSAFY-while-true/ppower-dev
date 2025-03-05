3e0b5191a0193bf8cefe0807a367efdc364ed50c
Lee Hyeon-seok <smothchoco@gmail.com>
2025-03-05 14:03:17 +0900

    Update BOJ30804.py

--- 202503_1/BOJ30804.py
+++ 202503_1/BOJ30804.py
@@ -1,28 +1,26 @@
-"""
-투포인터가 넘모 어려워용
-해시로 풀겠습니다
-"""
 from collections import defaultdict
 
+def max_fruit_length(fruits):
+    fruit_count = defaultdict(int)
+    left = 0
+    max_length = 0
+
+    for right in range(len(fruits)):
+        fruit_count[fruits[right]] += 1
+
+        # 과일 종류가 2개를 초과하면 left를 이동하여 범위를 줄임
+        while len(fruit_count) > 2:
+            fruit_count[fruits[left]] -= 1
+            if fruit_count[fruits[left]] == 0:
+                del fruit_count[fruits[left]]
+            left += 1
+
+        max_length = max(max_length, right - left + 1)
+
+    return max_length
+
+
 fruits_num = int(input())
-fruits = list(map(int, input.split()))
+fruits = list(map(int, input().split()))
 
-max_length = 0
-
-for front in range(fruits_num):
-    fruit_count = defaultdict(int)  # 남은 과일들의 개수를 저장할 해시맵
-    cur_fruits = 0  # 현재 남은 과일들의 종류 개수
-    
-    # front부터 시작해서 뒤에서 back 개를 뺄 때까지 탐색
-    for i in range(front, fruits_num):
-        fruit_count[fruits[i]] += 1
-        if fruit_count[fruits[i]] == 1:
-            cur_fruits += 1     # 새로운 과일 종류 등장
-
-        # 과일 종류가 2개 이하일 때만 최대 길이 갱신
-        if cur_fruits <= 2:
-            max_length = max(max_length, i - a + 1)
-        else:
-            break   # 과일 종류가 3개 이상이면 탐색 종료
-
-print(max_length)
+print(max_fruit_length(fruits))
