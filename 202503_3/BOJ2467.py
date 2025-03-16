ce098480bce268878ac38afd4289f0cd08d65696
ppower-dev <smothchoco@gmail.com>
2025-03-16 16:38:32 +0900

    BOJ2467.py 용액

--- 202503_3/BOJ2467.py
+++ 202503_3/BOJ2467.py
@@ -1,34 +1,26 @@
 def find_min_diff_coord(solutions: list) -> tuple:
     solutions.sort()
-    min_acid_idx = float('inf')
-    min_base_idx = float('-inf')
-    found_acid = False
-    print(solutions)
-    print(found_acid)
-    for i in range(len(solutions)):
-        if solutions[i] > 0:
-            found_acid = True
-            if i == 0:
-                return solutions[0], solutions[1]
-            min_acid_idx = i
-            min_base_idx = i - 1
-            break
-        
-        if not found_acid:
-            return solutions[-2], solutions[-1]
-    elem1 = (abs(solutions[min_acid_idx] + solutions[min_base_idx]), solutions[min_base_idx], solutions[min_acid_idx])
-    elem2 = (abs(solutions[min_acid_idx] + solutions[min_acid_idx + 1]), solutions[min_acid_idx], solutions[min_acid_idx + 1])
-    elem3 = (abs(solutions[min_base_idx] + solutions[min_base_idx - 1]), solutions[min_base_idx - 1], solutions[min_base_idx])
-    elem4 = (abs(solutions[0] + solutions[-1]), solutions[0], solutions[-1])
-    
-    min_elem = min(elem1, elem2, elem3, elem4, key=lambda x: x[0])
-    return min_elem[1], min_elem[2]
-    
+    left, right = 0, len(solutions) - 1
+    min_diff = float('inf')
+    best_pair = (solutions[left], solutions[right])
+
+    while left < right:
+        curr_sum = solutions[left] + solutions[right]
+
+        if abs(curr_sum) < min_diff:
+            min_diff = abs(curr_sum)
+            best_pair = (solutions[left], solutions[right])
+
+        if curr_sum > 0:
+            right -= 1
+        else:
+            left += 1
+
+    return best_pair
 
 if __name__ == '__main__':
     solution_num = int(input())
     solutions = list(map(int, input().split()))
-    min_diff_coord = find_min_diff_coord(solutions)
-    sol1, sol2 = min_diff_coord
+    sol1, sol2 = find_min_diff_coord(solutions)
 
     print(f'{sol1} {sol2}')
