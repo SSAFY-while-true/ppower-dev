d573102c0c357ada42690e898338f697777deb73
ppower-dev <smothchoco@gmail.com>
2025-04-06 18:50:50 +0900

    BOJ9019 DSLR

---
+++ 202504_1/BOJ9019.py
@@ -0,0 +1,13 @@
+from collections import deque
+
+
+"""
+다른 사람 블로그 봐도 뭔소린지 모르겠음...
+"""
+
+
+if __name__ == "__main__":
+    num_case = int(input())
+    for _ in range(num_case):
+        start, end = input().split()
+          
